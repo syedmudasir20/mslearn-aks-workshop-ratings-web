602da11717c994c95a958c55c1adf2e0c8edb5d6
syed mudasir <77150095+syedmudasir20@users.noreply.github.com>
2024-08-21 10:45:42 +0530

    Update Footer.vue

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -14,7 +14,7 @@
       <div class="col-lg-6">
       </div>
       <div class="col-lg-12 credits">
-        Azure Kubernetes Service (AKS) workshop
+        Azure Red Hat Openshift Service workshop
       </div>
       <div class="col-lg-6">
       </div>
